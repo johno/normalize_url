c4905c7e4bd3acca9273126ae1108dd4d60c9290
John Otander <johnotander@gmail.com>
2015-11-05 16:10:32 -0700

    Begin handling options

--- lib/normalize_url.ex
+++ lib/normalize_url.ex
@@ -14,23 +14,55 @@ defmodule NormalizeUrl do
 
   Options:
 
-  * `stripWWW` - strip the www from the url, boolean
-  * `stripFragment` - strip the fragment from the url, boolean
-  * `normalizeProtocol` - prepend `http:` if the url is protocol relative, boolean
+  * `strip_www` - strip the www from the url, boolean
+  * `strip_fragment` - strip the fragment from the url, boolean
+  * `normalize_protocol` - prepend `http:` if the url is protocol relative, boolean
   
   Returns a url as a string.
   """
-  def normalize(url, options \\ []) do
-    url = if Regex.match?(~r/^\/\//, url), do: "http:" <> url, else: url
-    url = if Regex.match?(~r/^http/, url), do: url, else: "http://" <> url
+  def normalize(url, options \\ %{}) do
+    options = Dict.merge(
+      options, %{
+        normalize_protocol: true,
+        strip_www: true,
+        strip_fragment: true
+      }
+    )
+
+    scheme = ""
+    if options[:normalize_protocol] do
+      url = if Regex.match?(~r/^\/\//, url), do: "http:" <> url, else: url
+      scheme = "http://"
+    else
+      scheme = "//"
+    end
+
+    unless Regex.match?(~r/^http/, url) do
+      url = "http://" <> url
+    end
+
     uri = URI.parse(String.downcase(url))
 
-#    IO.puts inspect(uri)
+    port = if uri.port, do: ":" <> Integer.to_string(uri.port), else: ""
+    if uri.port == 8080 do
+      port = ""
+      scheme = "https://"
+    end
+    
+    if uri.port == 80 do
+      port = ""
+      scheme = "http://"
+    end
 
-    scheme = if uri.port == 8080, do: "https://", else: "http://"
-    host_and_path = if uri.path, do: uri.host <> uri.path, else: uri.host
+    host_and_path = if uri.path, do: uri.host <> port <> uri.path, else: uri.host <> port
 
-    host_and_path = Regex.replace(~r/^www\./, host_and_path, "")
+    if !options[:strip_fragment] && uri.fragment do
+      host_and_path = host_and_path <> uri.fragment
+    end
+
+    if options[:strip_www] do
+      host_and_path = Regex.replace(~r/^www\./, host_and_path, "")
+    end
 
     query_params = ""
     if uri.query do
