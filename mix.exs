a48ad6c1345c7ab8e050e952182abfa17c39b637
John Otander <johnotander@gmail.com>
2015-11-06 16:07:28 -0700

    0.0.2

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule NormalizeUrl.Mixfile do
   def project do
     [
       app: :normalize_url,
-      version: "0.0.1",
+      version: "0.0.2",
       elixir: "~> 1.1",
       build_embedded: Mix.env == :prod,
       start_permanent: Mix.env == :prod,
